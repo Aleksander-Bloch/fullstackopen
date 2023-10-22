212f4c1f10e06b6b718e283485ce0f78468ce513
Aleksander Bloch <alekbloch21@gmail.com>
2023-10-22 10:34:29 +0200

    Finish unicafe step 4, 5, 6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -3,17 +3,36 @@ import { useState } from 'react';
 const Statistics = ({ good, neutral, bad }) => {
   const all = good + neutral + bad;
   const average = (good - bad) / all;
-  const positive = (good * 100) / all;
+  const positive = (good * 100) / all + ' %';
 
   return (
     <>
       <h2>Statistics</h2>
-      <p>all {all}</p>
-      <p>average {average}</p>
-      <p>positive {positive} %</p>
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="all" value={all} />
+          <StatisticLine text="average" value={average} />
+          <StatisticLine text="positive" value={positive} />
+        </tbody>
+      </table>
     </>
   );
 };
+
+const StatisticLine = ({ text, value }) => (
+  <tr>
+    <td>
+      {text} {value}
+    </td>
+  </tr>
+);
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
 
 const App = () => {
   // save clicks of each button to its own state
@@ -36,13 +55,14 @@ const App = () => {
   return (
     <div>
       <h1>Give feedback</h1>
-      <button onClick={handleGoodClick}>good</button>
-      <button onClick={handleNeutralClick}>neutral</button>
-      <button onClick={handleBadClick}>bad</button>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <Statistics good={good} neutral={neutral} bad={bad} />
+      <Button handleClick={handleGoodClick} text="good" />
+      <Button handleClick={handleNeutralClick} text="neutral" />
+      <Button handleClick={handleBadClick} text="bad" />
+      {good + neutral + bad > 0 ? (
+        <Statistics good={good} neutral={neutral} bad={bad} />
+      ) : (
+        <p>No feedback given</p>
+      )}
     </div>
   );
 };
