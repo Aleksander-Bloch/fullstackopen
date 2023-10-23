c45be69a7ea68acabca6b5694268f8a20fee18c3
Aleksander Bloch <alekbloch21@gmail.com>
2023-10-23 11:03:30 +0200

    Declare Course component as a separate module

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,35 +1,4 @@
-const Header = ({ course }) => <h2>{course}</h2>;
-
-const Total = ({ parts }) => {
-  const total = parts.reduce(
-    (prevTotal, part) => prevTotal + part.exercises,
-    0
-  );
-
-  return <b>total of {total} exercises</b>;
-};
-
-const Part = ({ part }) => (
-  <p>
-    {part.name} {part.exercises}
-  </p>
-);
-
-const Content = ({ parts }) => (
-  <>
-    {parts.map((part) => (
-      <Part key={part.id} part={part} />
-    ))}
-  </>
-);
-
-const Course = ({ course }) => (
-  <>
-    <Header course={course.name} />
-    <Content parts={course.parts} />
-    <Total parts={course.parts} />
-  </>
-);
+import Course from './components/Course';
 
 const App = () => {
   const courses = [
